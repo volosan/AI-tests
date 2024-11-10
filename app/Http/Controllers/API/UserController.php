1b7c5e3bdc4381ed60fbbb761462c12743acf883
Volodymyr Sanin <volodymyr.sanin@tallium.com>
2024-11-10 15:20:34 +0200

    only API resources

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -5,14 +5,12 @@ namespace App\Http\Controllers\API;
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\User\LoginRequest;
 use App\Http\Requests\User\StoreUserRequest;
 use App\Http\Requests\User\UpdateUserRequest;
 use App\Http\Resources\UserResource;
 use App\Models\User;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
-use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -43,20 +41,5 @@ class UserController extends Controller
     {
         $user->delete();
         return response()->json(null, 204);
-    }
-
-    public function login(LoginRequest $request): JsonResponse
-    {
-        if (!Auth::attempt($request->only('email', 'password'))) {
-            return response()->json(['message' => 'Invalid login credentials'], 401);
-        }
-
-        $user = $request->user();
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'Bearer',
-        ]);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('users', 'App\Http\Controllers\API\UserController');
-Route::post('login', 'App\Http\Controllers\API\UserController@login');
 
 Route::apiResource('posts', 'App\Http\Controllers\API\PostController');
 
